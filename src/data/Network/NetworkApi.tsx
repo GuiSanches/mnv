bd925501115f12a9dec41042929d90c7e3c5dcd1
Guilherme Sanches Flôres Queiroz <si.guilherme_queiroz@usp.br>
2022-05-23 15:33:43 -0300

    Update backend URL

--- src/data/Network/NetworkApi.tsx
+++ src/data/Network/NetworkApi.tsx
@@ -22,7 +22,7 @@ interface NetworkContainerResultResponse {
 }
 
 export default class NetworkApi implements NetworkRepository {
-    private API_URL = 'http://localhost:5000';
+    private API_URL = 'https://mnv-backend.herokuapp.com';
     private client: AxiosInstance;
 
     public constructor() {
